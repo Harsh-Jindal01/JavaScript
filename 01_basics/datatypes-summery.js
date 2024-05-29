1e0a18207a087665772c5b551672959698def8fd
Harsh <harshjindal955@gmail.com>
2024-05-29 14:23:51 +0530

    datatype-summery

---
+++ 01_basics/datatypes-summery.js
@@ -0,0 +1,25 @@
+// JavaScript is a dynamically typed language, which means that data types of variables are determined by the value they hold at runtime and can change throughout the program as we assign different values to them.
+
+// Primitive
+
+/*
+7 types: String, Number, Boolean, Undefined, null, Symbol(used to make any value unique), BigInt.
+
+    Number => number
+    String  => string
+    Boolean  => boolean
+    null  => object
+    undefined  =>  undefined
+    Symbol  =>  symbol
+    BigInt  =>  bigint
+*/
+
+// Reference (Non-Primitive) -- reference of address can be allocated in memory
+
+/*
+   Arrays  =>  object
+   Function  =>  function
+   Object  =>  object
+*/
+
+// Array, Objects, Functions.
